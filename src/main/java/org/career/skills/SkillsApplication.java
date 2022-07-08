30d55a4a259629c40015769f9b2f906ac81a44a2
yepenghao <1031053585@qq.com>
2022-07-09 00:29:59 +0800

    初始化

--- src/main/java/org/career/skills/SkillsApplication.java
+++ src/main/java/org/career/skills/SkillsApplication.java
@@ -3,11 +3,14 @@ package org.career.skills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yepenghao
+ */
 @SpringBootApplication
 public class SkillsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsApplication.class, args);
+    }
 
 }
